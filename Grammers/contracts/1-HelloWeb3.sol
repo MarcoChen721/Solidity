3eb1433dad96faba8fc3e10a4d85a497fff341d9
KudouShinichi <110006858+MarcoChen721@users.noreply.github.com>
2024-07-15 19:46:25 +0800

    Create 1-HelloWeb3.sol

---
+++ Grammers/contracts/1-HelloWeb3.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// contract HelloWeb2{
+//     string public Mystring = 'Hello Web2!';
+
+// }
+
+contract HelloWeb3{
+    string public _msg = "Hello Web3!";
+}
